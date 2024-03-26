0cac95098970f2a27b80f12b0461d1b3198fcacd
Hajjar <chajjar@MacBook-Pro-de-Hajjar.local>
2024-03-26 15:44:03 +0100

    r

---
+++ scalashop/src/main/scala/scalashop/HorizontalBoxBlur.scala
@@ -0,0 +1,60 @@
+package scalashop
+
+import org.scalameter.*
+
+object HorizontalBoxBlurRunner:
+
+  val standardConfig = config(
+    Key.exec.minWarmupRuns := 5,
+    Key.exec.maxWarmupRuns := 10,
+    Key.exec.benchRuns := 10,
+    Key.verbose := false
+  ) withWarmer(Warmer.Default())
+
+  def main(args: Array[String]): Unit =
+    val radius = 3
+    val width = 1920
+    val height = 1080
+    val src = Img(width, height)
+    val dst = Img(width, height)
+    val seqtime = standardConfig measure {
+      HorizontalBoxBlur.blur(src, dst, 0, height, radius)
+    }
+    println(s"sequential blur time: $seqtime")
+
+    val numTasks = 32
+    val partime = standardConfig measure {
+      HorizontalBoxBlur.parBlur(src, dst, numTasks, radius)
+    }
+    println(s"fork/join blur time: $partime")
+    println(s"speedup: ${seqtime.value / partime.value}")
+
+/** A simple, trivially parallelizable computation. */
+object HorizontalBoxBlur extends HorizontalBoxBlurInterface:
+
+  /** Blurs the rows of the source image `src` into the destination image `dst`,
+   *  starting with `from` and ending with `end` (non-inclusive).
+   *
+   *  Within each row, `blur` traverses the pixels by going from left to right.
+   */
+  def blur(src: Img, dst: Img, from: Int, end: Int, radius: Int): Unit =
+    for {
+      y <- from until end
+      x <- 0 until src.width
+    } dst(x, y) = boxBlurKernel(src, x, y, radius)
+
+  /** Blurs the rows of the source image in parallel using `numTasks` tasks.
+   *
+   *  Parallelization is done by dividing the source image `src` into
+   *  `numTasks` separate strips, where each strip is composed of some number of
+   *  rows.
+   */
+  def parBlur(src: Img, dst: Img, numTasks: Int, radius: Int): Unit =
+    val step = math.max(src.height / numTasks, 1)
+    val tasks = for {
+      start <- 0 until src.height by step
+    } yield task {
+      blur(src, dst, start, math.min(start + step, src.height), radius)
+    }
+    tasks.foreach(_.join())
+
